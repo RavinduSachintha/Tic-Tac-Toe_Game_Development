3e49a400c59168e3d961792956402262ddf00343
Sachintha <ravindusachintha53@gmail.com>
2018-05-27 07:30:45 +0530

    update project group

---
+++ TicTacToe-Web-Extended/public_html/game.js
@@ -0,0 +1,8 @@
+// global variables initializing
+var player = 1;
+var move = -1;
+var isFinished = false;
+var boardArray = [];
+
+var keys = document.querySelectorAll('#board span');
+var finalRes = document.getElementById("finalRes");
